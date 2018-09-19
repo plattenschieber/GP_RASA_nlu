1eaa0d194ea5cd2c32f100fb6b274068bd83415b
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2018-09-19 15:21:01 +0200

    Fix bamboo-spec pom.xml

--- bamboo-spec/src/test/java/io/archi-lab/chatbot/PlanSpecTest.java
+++ bamboo-spec/src/test/java/io/archi-lab/chatbot/PlanSpecTest.java
@@ -1,4 +1,4 @@
-package de.thkoeln.archilab;
+package io.archi-lab.chatbot;
 
 import com.atlassian.bamboo.specs.api.builders.plan.Plan;
 import com.atlassian.bamboo.specs.api.exceptions.PropertiesValidationException;
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class PlanSpecTest {
     @Test
-    public void checkYourPlanOffline() throws PropertiesValidationException {
+    public void checkYourPlanOffline() {
         Plan plan = new PlanSpec().createPlan();
 
         EntityPropertiesBuilders.build(plan);
