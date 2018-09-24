9bed3b6558d042019a80e1b4979da0e23e82ce55
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2018-09-24 13:22:34 +0200

    Add tagging and cleaning to spec

--- bamboo-specs/src/main/java/io/archi_lab/chatbot/PlanSpec.java
+++ bamboo-specs/src/main/java/io/archi_lab/chatbot/PlanSpec.java
@@ -15,8 +15,15 @@ import com.atlassian.bamboo.specs.api.builders.plan.branches.BranchCleanup;
 import com.atlassian.bamboo.specs.api.builders.plan.branches.PlanBranchManagement;
 import com.atlassian.bamboo.specs.api.builders.plan.configuration.ConcurrentBuilds;
 import com.atlassian.bamboo.specs.api.builders.project.Project;
-import com.atlassian.bamboo.specs.builders.task.*;
+import com.atlassian.bamboo.specs.api.builders.requirement.Requirement;
+import com.atlassian.bamboo.specs.builders.task.CheckoutItem;
+import com.atlassian.bamboo.specs.builders.task.DockerBuildImageTask;
+import com.atlassian.bamboo.specs.builders.task.DockerPushImageTask;
+import com.atlassian.bamboo.specs.builders.task.InjectVariablesTask;
+import com.atlassian.bamboo.specs.builders.task.ScriptTask;
+import com.atlassian.bamboo.specs.builders.task.VcsCheckoutTask;
 import com.atlassian.bamboo.specs.builders.trigger.BitbucketServerTrigger;
+import com.atlassian.bamboo.specs.model.task.InjectVariablesScope;
 import com.atlassian.bamboo.specs.util.BambooServer;
 
 @BambooSpec
@@ -33,32 +40,35 @@ public class PlanSpec {
             .description("Build the NLU app")
             .pluginConfigurations(new ConcurrentBuilds()
                 .useSystemWideDefault(false))
-            .stages(new Stage("Default Stage")
-                .jobs(new Job("Default Job",
-                    new BambooKey("JOB1"))
-                    .artifacts(new Artifact()
-                        .name("docker-compose-file")
-                        .copyPattern("docker-compose.yaml")
-                        .location("./docker")
-                        .shared(true)
-                        .required(true))
-                    .tasks(new VcsCheckoutTask()
-                            .description("Checkout Default Repository")
-                            .checkoutItems(new CheckoutItem().defaultRepository()),
-                        new DockerBuildImageTask()
-                            .description("Build the Docker image")
-                            .imageName("docker.nexus.gpchatbot.archi-lab.io/chatbot/nlu:latest")
-                            .useCache(true)
-                            .dockerfileInWorkingDir(),
-                        new ScriptTask()
-                            .description("Create commit hash variable file")
-                            .inlineBody("echo \"hello world\""),
-                            new ScriptTask()
-                                    .description("Create commit hash variable file")
-                                    .inlineBody("echo \"hello world AGAIN!\""),
-                        new DockerPushImageTask()
-                            .customRegistryImage("docker.nexus.gpchatbot.archi-lab.io/chatbot/nlu:latest")
-                            .defaultAuthentication())))
+                .tasks(new VcsCheckoutTask()
+                .description("Checkout the repository")
+                .checkoutItems(new CheckoutItem().defaultRepository()),
+            new ScriptTask()
+                .description("Create commit hash variable file")
+                .inlineBody("echo \"commit-hash=$(date +%s%N)\" > ./commit-hash"),
+            new InjectVariablesTask()
+                .description("Inject the commit hash variable")
+                .path("./commit-hash")
+                .namespace("inject")
+                .scope(InjectVariablesScope.RESULT),
+            new DockerBuildImageTask()
+                .description("Build the Docker image")
+                .imageName("docker.nexus.gpchatbot.archi-lab.io/chatbot/nlu")
+                .useCache(true)
+                .dockerfileInWorkingDir(),
+            new ScriptTask()
+                .description("Tag the Docker image with commit hash")
+                .inlineBody("docker tag docker.nexus.gpchatbot.archi-lab.io/chatbot/nlu docker.nexus.gpchatbot.archi-lab.io/chatbot/nlu:${bamboo.inject.commit-hash}"),
+            new DockerPushImageTask()
+                .customRegistryImage("docker.nexus.gpchatbot.archi-lab.io/chatbot/nlu")
+                .defaultAuthentication(),
+            new DockerPushImageTask()
+                .customRegistryImage("docker.nexus.gpchatbot.archi-lab.io/chatbot/nlu:${bamboo.inject.commit-hash}")
+                .defaultAuthentication(),
+            new ScriptTask()
+                .description("Remove old images from Nexus Docker repository")
+                .inlineBody("echo \"# Nexus Credentials\\nnexus_host = \\\"https://nexus.gpchatbot.archi-lab.io\\\"\\nnexus_username = \\\"bamboo\\\"\\nnexus_password = \\\"gpchatbot\\\"\\nnexus_repository = \\\"docker-hosted\\\"\" > .credentials\nnexus-cli image delete -name chatbot/nlu -keep 21"))
+        .requirements(new Requirement("system.builder.command.nexus-cli"))))
             .linkedRepositories("chatbot-nlu (master)")
 
             .triggers(new BitbucketServerTrigger())
@@ -69,16 +79,17 @@ public class PlanSpec {
     }
 
     public PlanPermissions planPermission() {
-        final PlanPermissions planPermission = new PlanPermissions(new PlanIdentifier("CHAT", "NLU"))
-            .permissions(new Permissions()
-                .userPermissions("jlengelsen", PermissionType.EDIT, PermissionType.VIEW, PermissionType.ADMIN, PermissionType.CLONE, PermissionType.BUILD)
-                .loggedInUserPermissions(PermissionType.VIEW)
-                .anonymousUserPermissionView());
+        final PlanPermissions planPermission =
+                new PlanPermissions(new PlanIdentifier("CHAT", "NLU")).permissions(new Permissions()
+                        .userPermissions("jlengelsen", PermissionType.EDIT, PermissionType.VIEW,
+                                PermissionType.ADMIN, PermissionType.CLONE, PermissionType.BUILD)
+                        .loggedInUserPermissions(PermissionType.VIEW)
+                        .anonymousUserPermissionView());
         return planPermission;
     }
 
     public static void main(String... argv) {
-        //By default credentials are read from the '.credentials' file.
+        // By default credentials are read from the '.credentials' file.
         BambooServer bambooServer = new BambooServer("https://bamboo.gpchatbot.archi-lab.io");
         final PlanSpec planSpec = new PlanSpec();
 
