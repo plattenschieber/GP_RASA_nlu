fa3cfc9e17cd48554c2647d9222549e151b83985
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2018-09-24 14:35:43 +0200

    Fix spec folder structure

--- bamboo-specs/src/test/java/io/archilab/gpchatbot/PlanSpecTest.java
+++ bamboo-specs/src/test/java/io/archilab/gpchatbot/PlanSpecTest.java
@@ -1,14 +1,15 @@
-package io.archi_lab.chatbot;
+package io.archilab.gpchatbot;
 
 import com.atlassian.bamboo.specs.api.builders.plan.Plan;
 import com.atlassian.bamboo.specs.api.util.EntityPropertiesBuilders;
 import org.junit.Test;
 
 public class PlanSpecTest {
-    @Test
-    public void checkYourPlanOffline() {
-        Plan plan = new PlanSpec().plan();
 
-        EntityPropertiesBuilders.build(plan);
-    }
+  @Test
+  public void checkYourPlanOffline() {
+    Plan plan = new PlanSpec().plan();
+
+    EntityPropertiesBuilders.build(plan);
+  }
 }
