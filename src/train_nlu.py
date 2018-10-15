9951b5c0f6cf030b9e0aaa6c083424fd9d7c90ec
Jann Deterling <jann.deterling@smail.th-koeln.de>
2018-10-15 11:01:55 +0200

    adds comments to train_nlu.py

--- src/train_nlu.py
+++ src/train_nlu.py
@@ -1,13 +1,28 @@
-from rasa_nlu.training_data import load_data
-from rasa_nlu.model import Trainer
 from rasa_nlu import config
-import datetime
+from rasa_nlu.model import Trainer
+from rasa_nlu.training_data import load_data
 
-def train(data = './intents', config_file = 'config.yaml', model_dir = 'models/', project = 'default'):
+
+def train(data='./intents', config_file='config.yaml', model_dir='models/', project='default'):
+    """ Trains the nlu on given config and data
+
+    Parameters:
+    ----------
+    data: str
+          path to training data
+    config_file: str
+                 path to config file
+    model_dir: str
+               path ot output model directory
+    project: str
+             name of the project, needed for model generation
+
+    """
     training_data = load_data(data)
     trainer = Trainer(config.load(config_file))
     trainer.train(training_data)
-    model_directory = trainer.persist(
+    trainer.persist(
         model_dir, project_name=project)
 
-train(project="damage_report_1.0.0") 
+
+train(project="damage_report_1.0.0")
